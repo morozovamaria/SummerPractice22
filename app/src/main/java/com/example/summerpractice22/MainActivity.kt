686024562bffa68625227a3dd0e07d165a964d89
Maria Morozova <mmorozova31@mail.ru>
2022-07-02 18:23:37 +0300

    Task_1

--- app/src/main/java/com/example/summerpractice22/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice22/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.summerpractice22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var user_field: EditText? = null
+    private var main_btn: Button? = null
+    private var result_info: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
